f387170fed4c624e9a8046cba71a252aeb017ef2
Niels J. de Wit <ndewit@gmail.com>
2018-07-27 23:12:59 +0200

    Working Shadertoy effect (albeit simple and shit slow)

--- code/plasma.cpp
+++ code/plasma.cpp
@@ -1,11 +1,24 @@
 
-// cookiedough -- simple marched plasma
+// cookiedough -- simple marched plasma (idea: https://www.shadertoy.com/view/ldSfzm)
+
+/*
+	to do:
+		- use as testbed for multi-threading & other optimizations
+		- make leaner where possible
+*/
 
 #include "main.h"
 // #include "plasma.h"
 #include "image.h"
 // #include "bilinear.h"
 #include "shadertoy-util.h"
+
+VIZ_INLINE float march(Vector3 point, float time)
+{
+	point.z += 5.f*time;
+	float sine = 0.2f*sinf(point.x-point.y);
+	return Vector3(sine+cosf(point.x*0.33f), sine+cosf(point.y*0.33f), sine+cosf(point.z*0.33f)).Length() - 0.8f; // FIXME: remove divide
+}
 
 bool Plasma_Create()
 {
@@ -18,14 +31,22 @@ void Plasma_Destroy()
 
 void Plasma_Draw(uint32_t *pDest, float time, float delta)
 {
-	for (unsigned iY = 0; iY < kResY; ++iY)
+	const Vector3 colMulA(0.3f, 0.15f, 0.1f);
+	const Vector3 colMulB(0.1f, 0.05f, 0.f);
+
+	for (float fY = 0; fY < kResY; ++fY)
 	{
-		for (unsigned iX = 0; iX < kResX; ++iX)
+		for (unsigned fX = 0; fX < kResX; ++fX)
 		{	
-			Vector2 coordinate = Shadertoy_ToCoord(iX, iY);
-			float aspectRatio = kAspect;
-			*pDest++ = 0xff00ff;
+			Vector3 direction((0.5f-fX)/kResX, (0.5f-fY)/kResY, 0.5f);
+			Vector3 origin = direction;
+
+			for (unsigned step = 0; step < 64; ++step)
+				origin += direction*march(origin, time);
+
+			Vector3 color = /* FIXME: abs() */ ( colMulA*march(origin+direction, time)+colMulB*march(origin*0.5f, time) ) * (8.f - origin.x/2.f);
+	
+			*pDest++ = Shadertoy::ToPixel(color);
 		}
 	}
 }
-
--- code/shadertoy-util.h
+++ code/shadertoy-util.h
@@ -1,31 +1,44 @@
 
 // cookiedough -- utility functions to easily port Shadertoy
 
+// missing:
+// - sign()
+
 #pragma once
 
-VIZ_INLINE const Vector2 Shadertoy_ToCoord(unsigned iX, unsigned iY)
+namespace Shadertoy
 {
-	float fX = (float) iX;
-	float fY = (float) iY;
-	fX *= 1.f/kResX;
-	fY *= 1.f/kResY;
-	return Vector2((fX-0.5f)*2.f, (fY-0.5f)*2.f);
-}
 
-VIZ_INLINE const Vector2 Shadertoy_ToCoord_RT(unsigned iX, unsigned iY)
-{
-	float fX = (float) iX;
-	float fY = (float) iY;
-	fX *= 1.f/kTargetResX;
-	fY *= 1.f/kTargetResY;
-	return Vector2((fX-0.5f)*2.f, (fY-0.5f)*2.f);
-}
+	VIZ_INLINE const Vector2 ToUV(unsigned iX, unsigned iY)
+	{
+		float fX = (float) iX;
+		float fY = (float) iY;
+		fX *= 1.f/kResX;
+		fY *= 1.f/kResY;
+		return Vector2((fX-0.5f)*2.f, (fY-0.5f)*2.f);
+	}
 
-/* IMPL MAKE FAST
-inline float sign(float x)
-{
-	if (x>0.f) return 1.f;
-	if (x<0.f) return -1.f;
-	else return 0.f;
+	VIZ_INLINE const Vector2 ToUV_RT(unsigned iX, unsigned iY)
+	{
+		float fX = (float) iX;
+		float fY = (float) iY;
+		fX *= 1.f/kTargetResX;
+		fY *= 1.f/kTargetResY;
+		return Vector2((fX-0.5f)*2.f, (fY-0.5f)*2.f);
+	}
+
+	VIZ_INLINE uint32_t ToPixel(Vector3 color)
+	{
+		// FIXME: use SSE!
+		color.x = fabsf(color.x);
+		color.y = fabsf(color.y);
+		color.z = fabsf(color.z);
+		if (color.x > 1.f) color.x = 1.f;
+		if (color.y > 1.f) color.y = 1.f;
+		if (color.z > 1.f) color.z = 1.f;
+		int R = int(color.x*255.f);
+		int G = int(color.y*255.f);
+		int B = int(color.z*255.f);
+		return B | G<<8 | R<<16;
+	}
 }
-*/
