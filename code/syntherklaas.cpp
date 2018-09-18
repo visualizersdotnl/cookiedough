3c0f46e5b876cfbd5134ad63b3c5f50752dd7496
Niels J. de Wit <ndewit@gmail.com>
2018-09-18 02:41:33 +0200

    FM part x

--- code/syntherklaas.cpp
+++ code/syntherklaas.cpp
@@ -2,6 +2,15 @@
 /*
 	See: http://ccrma.stanford.edu/software/snd/snd/fm.html
 	Important: Rocket not available (for now) in this mode, see main.cpp!
+
+	Missing:
+	- ADSR
+	- FIR
+	- Reverb
+	- More waveforms
+	- Multiple notes
+	- Presets
+	- MIDI support
 */
 
 #include "main.h"
@@ -19,16 +28,33 @@ void Syntherklaas_Destroy()
 {
 }
 
-// TODO: add channels, can easily do 4 at a time with SSE and LUTs.
+enum WaveForm
+{
+	kSine,
+	kSaw
+};
+
+struct FM_Note
+{
+	WaveForm form;
+
+	float amplitude;
+	float carrierFreq;
+	float modulationFreq;
+	float modulationIndex;
+
+	// To be initialized and updated during rendering.
+	float carrierPhase;
+	float modulationPhase;
+};
+
 struct FM
 {
+	// FIXME: unused!
 	float time;
 
-	// Single note
-	float toneAmp;
-	float carrierFreq;
-	float modFreq;
-	float modAmp;
+	// Single note/instrument.
+	FM_Note note;
 
 } static s_FM;
 
@@ -38,18 +64,25 @@ void Syntherklaas_Render(uint32_t *pDest, float time, float delta)
 {
 	s_FM.time = time;
 
-	// Define single note
-	s_FM.carrierFreq = 440.f;
-	s_FM.toneAmp = 1.f;
-	s_FM.modFreq = powf(kPI, kPI);
-	s_FM.modAmp = 1.f;
-
 	if (false == s_isReady)
 	{
+		// Define single note
+		s_FM.note.form = kSaw;
+		s_FM.note.carrierFreq = 440.f;
+		s_FM.note.amplitude = 1.f;
+		s_FM.note.modulationFreq = 4.f;
+		s_FM.note.modulationIndex = 2.f;
+
+		// Reset phases
+		s_FM.note.carrierPhase = 0.f;
+		s_FM.note.modulationPhase = 0.f;
+
 		Audio_Start_Stream();
+
 		s_isReady = true;
 	}
 
+	// Check for stream starvation
 	VIZ_ASSERT(true == Audio_Check_Stream());
 }
 
@@ -62,26 +95,37 @@ DWORD CALLBACK Syntherklaas_StreamFunc(HSTREAM hStream, void *pDest, DWORD lengt
 	const float time = s_FM.time;
 	
 	const float period = k2PI;
-	const float amplitude = s_FM.toneAmp;
-	const float modAmplitude = s_FM.modAmp;
+	const float amplitude = s_FM.note.amplitude;
+	const float modIndex = s_FM.note.modulationIndex;
 
-	float carrierStep = period*(s_FM.carrierFreq/kSampleRate);
-	float modStep = period*(s_FM.modFreq/kSampleRate);
-
-	// These are static for now since I'm just adding by number of sample instead of adding the timer.
-	static float carrierPhase = 0.f;
-	static float modPhase = 0.f;
+	float carrierStep = period*s_FM.note.carrierFreq/kSampleRate;
+	float modStep = period*s_FM.note.modulationFreq/kSampleRate;
 
 	float *pFloats = static_cast<float*>(pDest);
 	for (unsigned iSample = 0; iSample < numSamples; ++iSample)
 	{
 		// FM (Chowning)
-		const float tone = amplitude*sinf(carrierPhase + modAmplitude*sinf(modPhase));
+		const float modulation = modIndex*sinf(s_FM.note.modulationPhase);
+
+		float waveform;
+		switch (s_FM.note.form)
+		{
+			default:
+			case kSine:
+				waveform = sinf(s_FM.note.carrierPhase + modulation);
+				break;
+
+			case kSaw:
+				waveform = -1.f + fmodf(s_FM.note.carrierPhase + modulation, period);
+				break;
+		}
+
+		const float tone = amplitude*waveform;
 
 		*pFloats++ = tone;
 
-		carrierPhase += carrierStep;
-		modPhase += modStep;
+		s_FM.note.carrierPhase += carrierStep;
+		s_FM.note.modulationPhase += modStep;
 	}
 
 	return length;
