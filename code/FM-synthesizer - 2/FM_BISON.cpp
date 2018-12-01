c46c8be780ea3ffe2c91f8d25d52b12c3df93031
Niels J. de Wit <ndewit@gmail.com>
2018-12-01 02:41:19 +0100

    Update FM_BISON.cpp

--- code/FM-synthesizer - 2/FM_BISON.cpp
+++ code/FM-synthesizer - 2/FM_BISON.cpp
@@ -182,7 +182,7 @@ namespace SFM
 
 		FM_Patch &patch = s_parameters.patch;
 
-#if 1
+#if 0
 
 		/*
 			Test algorithm: single carrier & modulator
@@ -205,7 +205,7 @@ namespace SFM
 
 #endif
 
-#if 1
+#if 0
 
 		/*
 			Test algorithm: Volca FM algorithm #9
