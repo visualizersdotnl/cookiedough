658dc21e6a0732e3228c7111eea485391f74bcb5
Niels J. de Wit <ndewit@gmail.com>
2023-01-29 18:24:58 +0100

    Clumsily resubmitting SSE2NEON lib.

--- code/platform.h
+++ code/platform.h
@@ -9,7 +9,7 @@
 #elif __GNUC__
 
 	#if defined(__ARM_NEON) || defined(__ARM_NEON__)
-		#include "../3rdparty/sse2neon/sse2neon.h"
+		#include "../3rdparty/sse2neon-stripped/sse2neon.h"
 		#define FOR_ARM
 	#else // Most likely x64
 		#define FOR_INTEL
