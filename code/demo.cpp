b59c3b31949e6399534e588f61406af08e95585e
Niels J. de Wit <ndewit@gmail.com>
2023-03-26 06:44:53 +0200

    WHO?  WHO DOES NOT WANT TO WEAR THE RIBBON?

--- code/demo.cpp
+++ code/demo.cpp
@@ -598,9 +598,9 @@ bool Demo_Draw(uint32_t *pDest, float timer, float delta)
 				// TPB represent
 				memset32(g_renderTarget[0], 0xffffff, kResX*kResY);
 
-				// ribbon (F*CK this for now)
-//				const auto ribX = clampi(0.f, kResX, Rocket::geti(trackRibbonsTPB));
-//				MixSrc32S(g_renderTarget[0], s_pRibbons + ribX, kResX, kResY, 2160-kResX);
+				// ribbon (1 layer, FIXME)
+				const auto ribX = clampi(0.f, kResX, Rocket::geti(trackRibbonsTPB));
+				MixSrc32S(g_renderTarget[0], s_pRibbons + ribX, kResX, kResY, 2160-kResX);
 
 //				BlitSrc32(g_renderTarget[0] + ((kResX-800)/2) + ((kResY-600)/2)*kResX, g_pNytrikMexico, kResX, 800, 600);
 //				memcpy(g_renderTarget[0], g_pNytrikTPB, kOutputBytes);
