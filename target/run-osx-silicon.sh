2ae26bf85bf6e0bd1303c7bbf7da0fcc085c9612
Niels J. de Wit <ndewit@gmail.com>
2023-04-20 12:47:14 +0200

    Are we done now? I'll just upload this and wait for what Rik has to say about it.

--- target/run-osx-silicon.sh
+++ target/run-osx-silicon.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "*** Arrested Development is about to start! ***"
-echo "OSX is a pain when it comes to shipping self-contained Linux-style applications; please permit us to give execution rights to a few harmless and well-known libraries (in '/libs') so the demo can run. Thank you."
-echo "If not I understand, then simply press CTRL-C and wait for/check out the Youtube capture or Windows version (available in this same archive)."
+echo "ARRESTED DEVELOPMENT by Bypass featuring TPB"
+echo "OSX is a bit of a pain when it comes to shipping self-contained Linux-style applications, please permit us to give execution rights to our main executables and well-established dynamic libraries."
+echo "If you do not agree, please simply close this (terminal) window and either check Youtube or run the Windows version (available in this archive)."
+echo "Thank you for your patience."
 
 macallow () {
     if [[ $# -gt 0 ]]; then
         for f in $@; do
-            sudo xattr -d -r com.apple.quarantine $(realpath $f)
+            sudo xattr -d -r com.apple.quarantine "${f}"
+            echo "${f}"
         done
     else
         echo syntax: macallow [filename or wildcard]
@@ -17,4 +19,5 @@ macallow () {
 cd libs
 macallow *.dylib
 cd ..
+macallow "osx/cookiedough-silicon"
 ./osx/cookiedough-silicon
