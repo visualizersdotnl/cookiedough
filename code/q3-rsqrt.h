5e52cfff21dc4380b58016a46f5991ad2b4ccf66
Niels J. de Wit <ndewit@gmail.com>
2026-03-23 00:18:36 +0100

    Clarified the classic Quake III rsqrtf() a little for posterity

--- code/q3-rsqrt.h
+++ code/q3-rsqrt.h
@@ -1,5 +1,8 @@
 
-// cookiedough -- rsqrtf() implemention (inspired) by Carmack (Quake III); be warned: not necessarily faster given the instructions surrounding this!
+// cookiedough -- the rsqrtf() approximation that is chiefly a bit of 1990s lore by now
+
+// - (incomplete) explanation: https://betterexplained.com/articles/understanding-quakes-fast-inverse-square-root/
+// - don't use this if you need to be accurate, plus on most modern hardware you'd be hard pressed to find a code path for which it's still worth it
 
 #pragma once
 
@@ -8,14 +11,13 @@
 	#pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #endif
 
-// source: https://betterexplained.com/articles/understanding-quakes-fast-inverse-square-root/
-VIZ_INLINE float Q3_rsqrtf(float x)
+CKD_INLINE static float Q3_rsqrtf(float x)
 {
-	float xhalf = 0.5f * x;
-	int i = std::bit_cast<int>(x); // store floating-point bits in integer
-	i = 0x5f3759df - (i >> 1);     // initial guess for Newton's method
-	x = std::bit_cast<float>(i);   // convert new bits into float
-	x = x*(1.5f - xhalf*x*x);      // One round of Newton's method
+	const float xhalf = 0.5f*x;
+	int i = std::bit_cast<int>(x); 
+	i = 0x5f3759df - (i >> 1); // this is the initial guess (and the magic) that makes a single Newton iteration work (quasi)
+	x = std::bit_cast<float>(i);   
+	x = x*(1.5f - xhalf*x*x);  // single iteration of Newton's method (obviously repeating this would improve things)
 	return x;
 }
 
