ec4e613118ffa5db168b59fe8f766f6e04250683
Niels J. de Wit <ndewit@gmail.com>
2026-03-23 00:29:04 +0100

    The time I take to preserve 1990s obscurity with the care it deserves, right?

--- code/q3-rsqrt.h
+++ code/q3-rsqrt.h
@@ -1,8 +1,9 @@
 
-// cookiedough -- the rsqrtf() approximation that is chiefly a bit of 1990s lore by now
+// cookiedough -- the infamous rsqrtf() approximation
 
 // - (incomplete) explanation: https://betterexplained.com/articles/understanding-quakes-fast-inverse-square-root/
-// - don't use this if you need to be accurate, plus on most modern hardware you'd be hard pressed to find a code path for which it's still worth it
+// - don't use this if you need to be visually accurate (it's quite terrible when you're calculating normals for lighting for example)
+// - kept around for nostalgia (rather fitting given the scope of this project)
 
 #pragma once
 
@@ -13,11 +14,16 @@
 
 CKD_INLINE static float Q3_rsqrtf(float x)
 {
-	const float xhalf = 0.5f*x;
-	int i = std::bit_cast<int>(x); 
-	i = 0x5f3759df - (i >> 1); // this is the initial guess (and the magic) that makes a single Newton iteration work (quasi)
-	x = std::bit_cast<float>(i);   
-	x = x*(1.5f - xhalf*x*x);  // single iteration of Newton's method (obviously repeating this would improve things)
+	const float half = 0.5f*x;
+
+	// this is the initial guess (and the magic) that makes a single Newton iteration work (quasi)
+	int iX = std::bit_cast<int>(x); 
+	iX = 0x5f3759df - (iX>>1); 
+
+	// reinterpret back and iterate once (multiple would improve it a bit)
+	x = std::bit_cast<float>(iX);   
+	x = x*(1.5f - half*x*x); 
+
 	return x;
 }
 
