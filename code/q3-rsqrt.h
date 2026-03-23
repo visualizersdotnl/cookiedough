1a4b14777fd1cdcf4cc23d9a2566a1536c2b9ad0
Niels J. de Wit <ndewit@gmail.com>
2026-03-23 01:02:43 +0100

    Now I've fleshed this piece of legacy out to the point it's *enough*

--- code/q3-rsqrt.h
+++ code/q3-rsqrt.h
@@ -1,9 +1,16 @@
 
 // cookiedough -- the infamous rsqrtf() approximation
 
-// - (incomplete) explanation: https://betterexplained.com/articles/understanding-quakes-fast-inverse-square-root/
-// - don't use this if you need to be visually accurate (it's quite terrible when you're calculating normals for lighting for example)
-// - kept around for nostalgia (rather fitting given the scope of this project)
+/*
+	not all that useful anymore overall and definitely not for visually accuracy (it's horrible when for ex. calculating normals for lighting)
+	
+	- conceptual explanation: https://betterexplained.com/articles/understanding-quakes-fast-inverse-square-root/
+	- kept around for nostalgia (fits this project)
+
+	once the problem here was reduced to 'iX = constant - (iX>>1)' a fitting constant was approximated using an algorithm that
+	adjusted the constant by minimizing error for a practical range of values (which I guess can be deduced to some degree by simply looking
+	at what Carmack actually threw at it in Quake, if you've got the time for that sort of thing)
+*/
 
 #pragma once
 
@@ -16,8 +23,10 @@ CKD_INLINE static float Q3_rsqrtf(float x)
 {
 	const float half = 0.5f*x;
 
-	// this is the initial guess (and the magic) that makes a single Newton iteration work (quasi)
-	int iX = std::bit_cast<int>(x); 
+	// hint: the integer reinterpretation behaves roughly like log₂(x), so this line approximates x⁻¹ᐟ² in log space
+	int iX = std::bit_cast<int>(x);  
+
+	// this is the constant that makes a single Newton iteration converge fairly well
 	iX = 0x5f3759df - (iX>>1); 
 
 	// reinterpret back and iterate once (multiple would improve it a bit)
